81de69465b9f103c8e355e9cfe8034fd81dcf873
Thomas Schmidt <ithommy@me.com>
2018-03-24 12:58:07 +0100

    rem blank lines

--- app/src/main/java/com/example/android/musicplayer/activity/AlbumActivity.java
+++ app/src/main/java/com/example/android/musicplayer/activity/AlbumActivity.java
@@ -24,8 +24,6 @@ public class AlbumActivity extends AppCompatActivity {
         setContentView(R.layout.activity_album);
         setUpActionBar();
 
-
-
         Bundle bundle = getIntent().getExtras();
         if (bundle != null) {
 
